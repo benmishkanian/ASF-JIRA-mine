fc5aac0d6d401856a49b0273fb1574eeda383633
Ben <benmishkanian@ucdavis.edu>
2015-09-23 23:26:55 -0700

    reorganize script

--- analyzeData.R
+++ analyzeData.R
@@ -1,12 +1,28 @@
 library(RSQLite)
-dbConnection <- dbConnect(SQLite(), "sqlite.db")
 
 queryDatabase <- function(query) {
     dbGetQuery(dbConnection, query)
 }
 
+getContributors <- function() {
+    queryDatabase("select * from contributors")
+}
+
 getSummary <- function(isVolunteer) {
     isVolunteer <- as.numeric(isVolunteer)
     stopifnot(isVolunteer == 0 || isVolunteer == 1)
-    summary(queryDatabase(paste("select * from contributors where isVolunteer=",isVolunteer))[,c("issuesReported", "issuesResolved")])
+    print(summary(queryDatabase(paste("select * from contributors where isVolunteer=",isVolunteer))[,c("issuesReported", "issuesResolved")]))
+}
+
+analyzeData <- function() {
+    dbConnection <- dbConnect(SQLite(), "sqlite.db")
+    
+    contributors <- dbGetQuery(dbConnection, "select * from contributors")
+    cols <- c("issuesReported", "issuesResolved")
+    print("summary for volunteers")
+    print(summary(contributors[contributors$isVolunteer==1, cols]))
+    print("summary for employees")
+    print(summary(contributors[contributors$isVolunteer==0, cols]))
+    
+    invisible(dbDisconnect(dbConnection))
 }
