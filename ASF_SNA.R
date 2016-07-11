f0dabe5807beb0bf33157997acb692868280fcf0
Ben <benmishkanian@ucdavis.edu>
2016-07-10 17:40:20 -0700

    R: Add functions for obtaining company social network

--- ASF_SNA.R
+++ ASF_SNA.R
@@ -1,4 +1,5 @@
 library(network)
+library(sna)
 
 doQuery <- function(query) {
     dbGetQuery(dbConnection, query)
@@ -18,4 +19,12 @@ getContributorProjectCommitCount <- function() {
 
 getContributorCompanyProjectCommitCount <- function() {
     doQuery("select * from contributorcompanies cc inner join contributorprojectcommitcount cpcc on cc.contributors_id=cpcc.id")
+}
+
+getCompanyEdgeList <- function() {
+    doQuery("select cpcc1.company as company1, cpcc2.company as company2, cpcc1.commitcount+cpcc2.commitcount as combinedcommitcount from companyprojectcommitcount cpcc1 inner join companyprojectcommitcount cpcc2 on cpcc1.project=cpcc2.project where cpcc1.company != cpcc2.company;")
+}
+
+getCompanyNetwork <- function() {
+    network(getCompanyEdgeList(), directed = FALSE, matrix.type = "edgelist")
 }
